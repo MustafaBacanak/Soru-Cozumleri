73825aa3a5929088ad9b86916579c34937afbee6
Mustafa Bacanak <bacanak117@gmail.com>
2022-12-02 14:55:44 +0300

    A

--- src/main/java/d03_stringmanupilations/Soru_15.java
+++ src/main/java/d03_stringmanupilations/Soru_15.java
@@ -1,4 +1,31 @@
 package d03_stringmanupilations;
 
 public class Soru_15 {
+
+    public static void main(String[] args) {
+
+     /*
+           String gomlekFiyat = ‘$12.99’;
+           String kitapFiyat = ‘$35.99’;
+           Gömlek ve kitap fiyatlarının toplamını bulmak için gereken kodu yazınız.
+
+      */
+
+        String gomlekFiyat = "$1200.99";
+        String kitapFiyat = "$3500.99";
+
+        gomlekFiyat=gomlekFiyat.replace("$","");
+        kitapFiyat=kitapFiyat.replace("$","");
+
+        double x = Double.parseDouble(gomlekFiyat);
+        double y = Double.parseDouble(kitapFiyat);
+        double toplam = x+y;
+
+        System.out.printf("%5.2f",toplam);
+
+//      System.out.println();
+//      System.out.println(String.format("%.2f",toplam));
+
+
+    }
 }
--- src/main/java/d03_stringmanupilations/Soru_17.java
+++ src/main/java/d03_stringmanupilations/Soru_17.java
@@ -1,4 +1,19 @@
 package d03_stringmanupilations;
 
 public class Soru_17 {
+    public static void main(String[] args) {
+
+    /*
+        Bir String’ de kullanılan noktalama işaretlerinin sayısını bulmak için bir kod yazınız.
+        String s = "Vay be! Ali 13 yaşında ama üniversite öğrencisi.";
+    */
+
+        String s = "Vay be! Ali 13 yaşında ama üniversite öğrencisi.";
+
+        s=s.replaceAll("[^\\p{Punct}]","");
+        System.out.println(s);
+        System.out.println(s.length());
+
+
+    }
 }
